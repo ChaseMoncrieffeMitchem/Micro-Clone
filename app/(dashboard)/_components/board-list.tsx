1dca2266ed98ca4347fb22c3af5cb50a35e2edc8
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-05-27 10:12:02 -0500

    Made board list with Searchparam props

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = []; // TODO: Change to API call
+
+  if (!data?.length && query.search) {
+    return <div>Try searching for something else</div>;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <div>No favorites</div>;
+  }
+
+  if (!data?.length) {
+    return <div>No boards at all</div>;
+  }
+
+  return <div>{JSON.stringify(query)}</div>;
+};
+
+export default BoardList;
